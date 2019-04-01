62f12914e13dbdc7d483c618248547ee0142a37e
Max Vaysburd <max.vays@gmail.com>
2019-03-31 21:11:46 -0400

    Create starting files

---
+++ OrderedList_inArraySlots.java
@@ -0,0 +1,13 @@
+public class OrderedList_inArraySlots{
+
+  List_inArraySlots orderedList;
+
+  public OrderedList_inArraySlots(){
+    orderedList = new List_inArraySlots();
+  }
+
+  public String toString(){
+    return orderedList.toString;
+  }
+
+}
---
+++ UserOfOrderedList.java
@@ -0,0 +1,18 @@
+public class UserOfOrderedList{
+/*
+List of required methods/constructors:
+no-arg constructor
+toString
+add
+get
+set
+remove*/
+
+  public static void main(String[] args){
+
+      OrderedList_inArraySlots list = new OrderedList_inArraySlots();
+      System.out.println(list);
+
+  }
+
+}
